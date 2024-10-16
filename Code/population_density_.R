1215beaf3e189a2c8b1204838a2d4f2044a31186
FinnsTT <sofiobissoni@gmail.com>
2024-10-16 11:59:36 +0200

    Update population_density_.R

--- Code/population_density_.R
+++ Code/population_density_.R
@@ -6,6 +6,10 @@ install.packages("spatstat")
 #recalling the package
 library(spatstat)
 
+# let's use the bei data:
+# data description:
+# https://CRAN.R-project.org/package=spatstat  #non ho capito come si trova bei
+
 #dataset
 bei 
 
@@ -32,4 +36,31 @@ densitymap   #i can just write this because i have already assign
 plot(densitymap)  
 points(bei)
 points(bei, col="green")
+
+
+plot(bei)
+densitymap <- density(bei)
+plot(densitymap)
+plot(bei)
+plot(densitymap)
+plot(bei)  #non ho capito cosa sta succedendo 15 mins into the recording
+
+plot(densitymap)
+points(bei, col=green, cex=.5) #per cambiare dimensione e colore 
+
+elev <- bei.extrs [[1]] #posso anche scrivere elev<-bei.extra$elev ma almeno così non devo ricordarmi il nome ma boh non funziona
+
+#passing to points to a counting surface
+#par function r to build multifraim to maps one besides the other 
+par(mfrow=c(1,2))  #mf = multiframe, row = how many rows, ne ho due perchè elevation e density li devo mettere sulla stessa fila. I have now, and 2 for the 2 colors. i can use C for concatenate the elemenents
+#devo scrivere questo codice e poi i due plot sotto
+plot(elev)
+plot(densitymap)
+
+#esercizio, make a multiframe with maps one non top of the other 
+par(mfrow=c(2,1))
+plot(elev)
+plot(densitymap)
+
+dev.off() #one frined to clear graphs
 
