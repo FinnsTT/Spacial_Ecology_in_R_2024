eeb8b8d05293500bb02fb71756b60ec1d733c89d
FinnsTT <sofiobissoni@gmail.com>
2024-10-17 15:52:24 +0200

    Update 03_population_distribution.R

--- Code/03_population_distribution.R
+++ Code/03_population_distribution.R
@@ -27,9 +27,44 @@ plot(rana) #let's plot
 
 press <- rana[rana$Occurrence==1] #rana is the dataset, the elements #from rana go to occurrence and send all the dataset wher occurence = 1 select all the dataset in line with this condition. this is called press
 
-#ESERCIZIO plot in a multiframe the rana dataset beside the press dataset
+#ESERCIZIO 1 plot in a multiframe the rana dataset beside the press dataset
 par(mfrow=c(1,2))
 plot(rana)
-plot(press) 
+plot(pres) 
 
+#ESERCIZIO 2 select data from rana with only absences
+abse <- rana[rana$Occurrence==0]
+abse$Occurrence #to see the absence obcurrence  #there may be an error called observer byassed, potresti non aver visto l'animale
+
+plot(abse)
+
+#ESERCIZIO 3 #plot in a multiframe presence beside absence
+par(mfrow=c(1,2))
+plot(pres)
+plot(abse) 
+
+#ESERCIZIO 4 #plot in a multiframe presence on top of absence
+par(mfrow=c(2,1))
+plot(pres)
+plot(abse) 
+
+#ESERCIZIO 5 #plot the presence in blue toghether with absences in red
+plot(pres, col="blue4")
+points(abse, col="red3")
+
+plot(pres, col="blue4", pch=19, cex=1)
+points(abse, col="red3", pch=19, cex=1)
+
+
+#we can now try to undestand ecologically why frogs are somewhere and not somewhere else 
+#let's take the previous function (change the name) and use it to understand that
+
+elev <- system.file("external/elevation.asc", package="sdm") #elevation.asc perchè è una funzione non un vettore
+elevmap <- rast(elev)    #map of the elevation map 
+
+#ESERCIZIO6 #change the colors
+colorRampPalette (c("blue4","mediumseagreen","mistyrose3")) (100) 
+cl <- colorRampPalette (c("blue4","mediumseagreen","mistyrose3")) (100) 
+plot(elevmap, col=cl)
+points(pres, pch=19)
 
