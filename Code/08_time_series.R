1166163100c62fe4a25581fe607ff9b6d0508472
FinnsTT <sofiobissoni@gmail.com>
2024-11-27 11:58:01 +0100

    Update 08_time_series.R

--- Code/08_time_series.R
+++ Code/08_time_series.R
@@ -1,43 +1,39 @@
 # time series analysis
 
-library(imageRy)
+library(imageRy) #all the files that comes out of imageRy starts with "im."
 library(terra)
 
-im.list()
+im.list() #listing all the available files. #EN 
 
 # import the data
-EN01 <- im.import("EN_01.png")
+EN01 <- im.import("EN_01.png")  #directly plotted, downloaded by esa
 EN13 <- im.import("EN_13.png")
 
-par(mfrow=c(2,1))
+par(mfrow=c(2,1)) 
 im.plotRGB.auto(EN01)
 im.plotRGB.auto(EN13)
 
-# using the first element (band) of images
-dif = EN01[[1]] - EN13[[1]]
+# using the first element (band) of images #overlapping and making a difference ????
+difEN = EN01[[1]] - EN13[[1]]   #= instead of <- because it's math?
 
 # palette
-cldif <- colorRampPalette(c("blue", "white", "red")) (100)
-plot(dif, col=cldif)
+cldifEN <- colorRampPalette(c("blue", "white", "red")) (100)
+plot(difEN, col=cldif)
 
 
-### New example: temperature in Greenland
+## EXAMPLE 2: temperature in Greenland
+# how to import severla files altogether, by using part of the name of the data that is common to all of them
+gr <- im.import("greenland") #we can now see the differences
 
-# how to import severla files altogether
-greenland <- im.import("greenland")
-
-plot(green)
+plot(gr)
 
 par(mfrow=c(1,2))
-plot(greenland[[1]], col=viridis(100))
-plot(greenland[[4]], cpò=viridis(100))
+plot(gr[[1]])
+plot(gr[[4]])
 
-plot(greenland[[1]], col=rocket(100))
-plot(greenland[[4]], cpò=rocket(100))
-
-# Exercise: make the differencxe between the first and the final elemnts of the stack
-difg <- greenland[[4]] - greenland[[1]]
-plot(difg)
+difgr <- gr[[1]] - gr [[4]]  #to see them toghether
+dev.off()
+plot(difgr) #make the differencxe between the first and the final elemnts of the stack
 
 # Exercise: make a RGB plot using different years
-im.plotRGB(green, r=1, g=2, b=3)
+im.plotRGB(green, r=1, g=2, b=3) #if you have higher temperature in 2005 the imagines will be of the first colors while if it's hotter in 2015 they will be green
